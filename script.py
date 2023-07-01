d0e305cf74b29f161aeaa2fd2461a0697901d77c
aa-ag <aaron.aguerrevere@gmail.com>
2023-07-01 13:03:00 -0500

    Add example of encapsulation with private attribute

---
+++ script.py
@@ -0,0 +1,16 @@
+class Base:
+    def __init__(self):
+        self.a = "GeeksforGeeks"
+        self.__c = "GeeksforGeeks"
+
+
+class Derived(Base):
+    def __init__(self):
+        Base.__init__(self)
+        print("Calling private member of base class: ")
+        print(self.__c)
+
+
+obj1 = Base()
+print(obj1.a)
+print(obj1.c)  # 'Base' object has no attribute 'c'
