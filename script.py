babec2938bc918a6d23bd495e1041da42c0d8bfc
aa-ag <aaron.aguerrevere@gmail.com>
2023-07-03 11:47:28 -0500

    Add examples of __repr__ or __str__ methods

--- script.py
+++ script.py
@@ -1,6 +1,16 @@
-class MyClass:
-    __hidden_variable = 10
+class ThisClass:
+    def __init__(self, variable_a, variable_b):
+        self.variable_a = variable_a
+        self.variable_b = variable_b
+
+    def __repr__(self):
+        return "Class a:%s b:%s" % (self.variable_a, self.variable_b)
+
+    def __str__(self):
+        return "From __str__(): a is %s, " \
+            "b is %s" % (self.variable_a, self.variable_b)
 
 
-myObject = MyClass()
-print(myObject._MyClass__hidden_variable)
+c = ThisClass(123, 456)
+print(c)  # calls __str__()
+print([c])  # calls __repr__()
